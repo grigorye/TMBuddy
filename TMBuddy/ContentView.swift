7a194944954a7e416a42ee2fc9ab7e14341d8ea2
Grigory Entin <grigory.entin@gmail.com>
2022-01-12 14:34:40 +0100

    Added compatibility with macOS 10.15.

--- TMBuddy/ContentView.swift
+++ TMBuddy/ContentView.swift
@@ -3,15 +3,35 @@ import Foundation
 
 struct ContentView: View {
     var body: some View {
-        let appName = FileManager.default.displayName(atPath: Bundle.main.bundlePath)
-        let finderName = FileManager.default.displayName(atPath: "/System/Library/CoreServices/Finder.app")
-        Text(
-            """
-            Please see [Instructions](https://github.com/grigorye/TMBuddy#installation) on how to enable \(appName) in \(finderName).
-            """
-        )
+        prompt()
             .fixedSize()
             .padding()
+    }
+}
+
+@ViewBuilder
+private func prompt() -> some View {
+    if #available(macOS 12, *) {
+        Text("Please see [Instructions](https://github.com/grigorye/TMBuddy#installation) on how to enable \(appName) in \(finderName).")
+    } else {
+        Button(
+            action: {
+                NSWorkspace.shared.open(URL(string: "https://github.com/grigorye/TMBuddy#installation")!)
+            },
+            label: {
+                Text("How to enable \(appName) in \(finderName).")
+                    .underline()
+                    .foregroundColor(Color.blue)
+            }
+        )
+            .buttonStyle(PlainButtonStyle())
+            .onHover { inside in
+                if inside {
+                    NSCursor.pointingHand.push()
+                } else {
+                    NSCursor.pop()
+                }
+            }
     }
 }
 
---
+++ TMBuddy/DisplayNames.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+let appName = FileManager.default.displayName(atPath: Bundle.main.bundlePath)
+let finderName = FileManager.default.displayName(atPath: "/System/Library/CoreServices/Finder.app")
