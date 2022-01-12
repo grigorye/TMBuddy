b5d09300533939bbb0933eceb83bdf5af49e4ee7
Grigory Entin <grigory.entin@gmail.com>
2022-01-12 23:13:27 +0100

    Integrated SandboxAccessView.

--- TMBuddy/ContentView.swift
+++ TMBuddy/ContentView.swift
@@ -1,17 +1,18 @@
 import SwiftUI
 import Foundation
 
+let appName = FileManager.default.displayName(atPath: Bundle.main.bundlePath)
+let finderName = FileManager.default.displayName(atPath: "/System/Library/CoreServices/Finder.app")
+
 struct ContentView: View {
+    
     var body: some View {
-        let appName = FileManager.default.displayName(atPath: Bundle.main.bundlePath)
-        let finderName = FileManager.default.displayName(atPath: "/System/Library/CoreServices/Finder.app")
-        Text(
-            """
-            Please see [Instructions](https://github.com/grigorye/TMBuddy#installation) on how to enable \(appName) in \(finderName).
-            """
-        )
-            .fixedSize()
-            .padding()
+        VStack {
+            Text("Please see [Instructions](https://github.com/grigorye/TMBuddy#installation) on how to enable \(appName) in \(finderName).")
+                .fixedSize()
+            SandboxAccessView()
+        }
+        .padding()
     }
 }
 
