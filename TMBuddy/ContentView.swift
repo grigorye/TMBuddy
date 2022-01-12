4964a39a151e489568091dc3c1f6f7f67d37144e
Grigory Entin <grigory.entin@gmail.com>
2022-01-12 11:43:44 +0100

    Dropped the message about no need to run the app.

--- TMBuddy/ContentView.swift
+++ TMBuddy/ContentView.swift
@@ -7,7 +7,6 @@ struct ContentView: View {
         let finderName = FileManager.default.displayName(atPath: "/System/Library/CoreServices/Finder.app")
         Text(
             """
-            There's no need to run this app manually.
             Please see [Instructions](https://github.com/grigorye/TMBuddy#installation) on how to enable \(appName) in \(finderName).
             """
         )
