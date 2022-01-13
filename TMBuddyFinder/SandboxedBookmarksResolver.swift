09d88616048ce64ca088c5ea8823ea7854c5423c
Grigory Entin <grigory.entin@gmail.com>
2022-01-13 01:34:27 +0100

    Extracted resolveBookmarks.

--- TMBuddyFinder/SandboxedBookmarksResolver.swift
+++ TMBuddyFinder/SandboxedBookmarksResolver.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class SandboxedBookmarksResolver {
     
-    init(handleResolvedURLs: @escaping ([URL]) -> Void) {
+    init(handleResolvedBookmarkURLs: @escaping ([URL]) -> Void) {
         observer = UserDefaultsObserver(defaults: sharedDefaults, key: DefaultsKey.sandboxedBookmarks.rawValue, options: [.new, .initial]) { change in
             
             guard let changeBookmarks = change![.newKey] as? [Data]? else {
@@ -13,21 +13,25 @@ class SandboxedBookmarksResolver {
             guard let bookmarks = changeBookmarks else {
                 return
             }
-            let resolvedURLs: [URL] = bookmarks.compactMap { bookmark in
-                do {
-                    var isStale = false
-                    let url = try URL(resolvingBookmarkData: bookmark, bookmarkDataIsStale: &isStale)
-                    dump((url, isStale: isStale), name: "resolvedSandboxedBookmark")
-                    return url
-                } catch {
-                    dump((error, bookmark: bookmark), name: "sandboxBookmarkResolutionFailed")
-                    return nil
-                }
-            }
             
-            handleResolvedURLs(resolvedURLs)
+            let resolvedBookmarkURLs = resolveBookmarks(bookmarks)
+            handleResolvedBookmarkURLs(resolvedBookmarkURLs)
         }
     }
     
     private let observer: UserDefaultsObserver
+}
+
+func resolveBookmarks(_ bookmarks: [Data]) -> [URL] {
+    bookmarks.compactMap { bookmark in
+        do {
+            var isStale = false
+            let url = try URL(resolvingBookmarkData: bookmark, bookmarkDataIsStale: &isStale)
+            dump((url, isStale: isStale), name: "resolvedSandboxedBookmark")
+            return url
+        } catch {
+            dump((error, bookmark: bookmark), name: "sandboxBookmarkResolutionFailed")
+            return nil
+        }
+    }
 }
