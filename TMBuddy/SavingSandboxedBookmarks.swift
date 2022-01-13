ec7f6220a017acef2116775d58f4a853122026d4
Grigory Entin <grigory.entin@gmail.com>
2022-01-13 07:13:33 +0100

    Segregated SavingSandboxingBookmarks.

---
+++ TMBuddy/SavingSandboxedBookmarks.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func saveScopedSandboxedBookmark(urls: [URL], in defaults: UserDefaults) throws {
+    let bookmarks: [Data] = try urls.map { url in
+        let bookmark = try url.bookmarkData(options: [.withSecurityScope, .securityScopeAllowOnlyReadAccess])
+        dump((path: url.path, bookmark), name: "bookmark")
+        return bookmark
+    }
+    
+    defaults.scopedSandboxedBookmarks = bookmarks
+}
+
+func saveSandboxedBookmark(urls: [URL], in defaults: UserDefaults) throws {
+    let bookmarks: [Data] = try urls.map { url in
+        let bookmark = try url.bookmarkData(options: [])
+        dump((path: url.path, bookmark), name: "bookmark")
+        return bookmark
+    }
+    
+    defaults.sandboxedBookmarks = bookmarks
+}
+
+private let defaults = UserDefaults()
+
+extension Array where Element == URL {
+    var paths: [String] {
+        map { $0.path }
+    }
+}
