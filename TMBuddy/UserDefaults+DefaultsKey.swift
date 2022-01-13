ca75beedc2c4dcc87e25b457f0d35740c1c65aaf
Grigory Entin <grigory.entin@gmail.com>
2022-01-13 15:56:27 +0100

    Switched UserDefaults+DefaultsKey to generic implementation.

--- TMBuddy/UserDefaults+DefaultsKey.swift
+++ TMBuddy/UserDefaults+DefaultsKey.swift
@@ -2,19 +2,27 @@ import Foundation
 
 extension UserDefaults {
     
-    func data(forKey key: DefaultsKey) -> Data? {
+    func data<DefaultsKey>(forKey key: DefaultsKey) -> Data?
+    where DefaultsKey: RawRepresentable, DefaultsKey.RawValue == String
+    {
         data(forKey: key.rawValue)
     }
     
-    func array(forKey key: DefaultsKey) -> [Data]? {
+    func array<DefaultsKey>(forKey key: DefaultsKey) -> [Data]?
+    where DefaultsKey: RawRepresentable, DefaultsKey.RawValue == String
+    {
         array(forKey: key.rawValue) as? [Data]
     }
 
-    func set(_ data: Data?, forKey key: DefaultsKey) {
+    func set<DefaultsKey>(_ data: Data?, forKey key: DefaultsKey)
+    where DefaultsKey: RawRepresentable, DefaultsKey.RawValue == String
+    {
         set(data, forKey: key.rawValue)
     }
     
-    func set(_ data: [Data]?, forKey key: DefaultsKey) {
+    func set<DefaultsKey>(_ data: [Data]?, forKey key: DefaultsKey)
+    where DefaultsKey: RawRepresentable, DefaultsKey.RawValue == String
+    {
         set(data as [NSData]?, forKey: key.rawValue)
     }
 }
