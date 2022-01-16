06fd475c88b9b4bc6ed9e27e91267c72c6039ec4
Grigory Entin <grigory.entin@gmail.com>
2022-01-16 01:21:23 +0100

    Added extensions for String ranges.

---
+++ Targets/TMBuddyShared/Helpers/String+Ranges.swift
@@ -0,0 +1,16 @@
+// Borrowed from https://stackoverflow.com/a/46133083/1859783
+
+extension String {
+    
+    subscript(_ range: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let end = index(start, offsetBy: min(self.count - range.lowerBound,
+                                             range.upperBound - range.lowerBound))
+        return String(self[start..<end])
+    }
+    
+    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        return String(self[start...])
+    }
+}
