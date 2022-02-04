60d4369d4bf4f490ee6f83b61e2aef92b2bf04a6
Grigory Entin <grigory.entin@gmail.com>
2022-02-04 01:50:49 +0100

    Made Install button always available in SMJobBless checkpoint view.

--- Targets/TMBuddy/Sources/Checkpoints/SMJobBless/SMJobBlessCheckpointView.swift
+++ Targets/TMBuddy/Sources/Checkpoints/SMJobBless/SMJobBlessCheckpointView.swift
@@ -28,15 +28,13 @@ struct SMJobBlessCheckpointView: View {
         }()
         
         CheckpointView(title: "Time Machine settings writing", subtitle: nil, value: value, readiness: readiness) {
-            if checkpointProvider.state == .toolNotInstalled {
-                Button("Install Helper") {
-                    let message = "\(appName) needs your permission to manipulate the Time Machine path exclusion list."
-                    let icon = Bundle.main.url(forResource: "bless", withExtension: "png")
-                    do {
-                        try LaunchdManager.authorizeAndBless(message: message, icon: icon)
-                    } catch {
-                        NSApp.presentError(error)
-                    }
+            Button("Install Helper") {
+                let message = "\(appName) needs your permission to manipulate the Time Machine path exclusion list."
+                let icon = Bundle.main.url(forResource: "bless", withExtension: "png")
+                do {
+                    try LaunchdManager.authorizeAndBless(message: message, icon: icon)
+                } catch {
+                    NSApp.presentError(error)
                 }
             }
         }
