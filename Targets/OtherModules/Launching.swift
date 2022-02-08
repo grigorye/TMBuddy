45b399e24f9bd67653644d61e095569e41351691
Grigory Entin <grigory.entin@gmail.com>
2022-02-08 03:35:39 +0100

    Switched to synchronous runAndCaptureOutput.

--- Targets/OtherModules/Launching.swift
+++ Targets/OtherModules/Launching.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func runAndCaptureOutput(executableURL: URL, arguments: [String] = []) -> Task<Data, Error> {
+func runAndCaptureOutput(executableURL: URL, arguments: [String] = []) throws -> Data {
     let standardOutput = Pipe()
     let standardError = Pipe()
     
@@ -15,44 +15,42 @@ func runAndCaptureOutput(executableURL: URL, arguments: [String] = []) -> Task<D
         assert($0 == process)
     }
     
-    return Task { () -> Data in
-        process.launch()
-        
-        dump((executable: executableURL.path, arguments: arguments), name: "launched")
-        
-        process.waitUntilExit()
-        
-        dump((executable: executableURL.path, arguments: arguments, status: process.terminationStatus), name: "terminated")
-        
-        guard process.terminationStatus == ERR_SUCCESS else {
-            let standardError: String = {
-                let data = standardError.fileHandleForReading.readDataToEndOfFile()
-                guard let standardError = String(data: data, encoding: .utf8) else {
-                    dump((executable: executableURL.path, arguments: arguments, standarErrorData: data), name: "standardErrorUTF8ConversionFailed")
-                    return ""
-                }
-                return standardError
-            }()
-            enum Error: Swift.Error {
-                case processFailed(
-                    executable: String,
-                    arguments: [String],
-                    terminationStatus: Int32,
-                    standardError: String
-                )
+    process.launch()
+    
+    dump((executable: executableURL.path, arguments: arguments), name: "launched")
+    
+    process.waitUntilExit()
+    
+    dump((executable: executableURL.path, arguments: arguments, status: process.terminationStatus), name: "terminated")
+    
+    guard process.terminationStatus == ERR_SUCCESS else {
+        let standardError: String = {
+            let data = standardError.fileHandleForReading.readDataToEndOfFile()
+            guard let standardError = String(data: data, encoding: .utf8) else {
+                dump((executable: executableURL.path, arguments: arguments, standarErrorData: data), name: "standardErrorUTF8ConversionFailed")
+                return ""
             }
-            throw dump(
-                Error.processFailed(
-                    executable: executableURL.path,
-                    arguments: arguments,
-                    terminationStatus: process.terminationStatus,
-                    standardError: standardError
-                ),
-                name: "error"
+            return standardError
+        }()
+        enum Error: Swift.Error {
+            case processFailed(
+                executable: String,
+                arguments: [String],
+                terminationStatus: Int32,
+                standardError: String
             )
         }
-        
-        let data = standardOutput.fileHandleForReading.readDataToEndOfFile()
-        return data
+        throw dump(
+            Error.processFailed(
+                executable: executableURL.path,
+                arguments: arguments,
+                terminationStatus: process.terminationStatus,
+                standardError: standardError
+            ),
+            name: "error"
+        )
     }
+    
+    let data = standardOutput.fileHandleForReading.readDataToEndOfFile()
+    return data
 }
