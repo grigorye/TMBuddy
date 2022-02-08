0f8b94c812805ecf23ad398231ac228cf109875f
Grigory Entin <grigory.entin@gmail.com>
2022-02-08 17:22:12 +0100

    Switched TMUtilHelperXPCImp to synchronous implementation.

--- Targets/TMBuddyTMUtilHelper/Sources/TMUtilHelperXPCImp.swift
+++ Targets/TMBuddyTMUtilHelper/Sources/TMUtilHelperXPCImp.swift
@@ -2,20 +2,15 @@ import Foundation
 
 final class TMUtilHelperXPCImp: CommonHelperXPCImp, TMUtilHelperXPC {
     
-    func setExcludedByPath(_ value: Bool, paths: [String], completion: @escaping (Error?) -> Void) {
-        let task = Task { try await setExcludedByPath(value, paths: paths) }
-        Task {
-            let result = await task.result
-            switch result {
-            case .success(()):
-                completion(nil)
-            case let .failure(error):
-                completion(error)
-            }
+    func setExcludedByPath(_ value: Bool, paths: [String], reply: @escaping (Error?) -> Void) {
+        let result = Result {
+            try setExcludedByPath(value, paths: paths)
         }
+        dump(result, name: "result")
+        result.send(to: reply)
     }
     
-    func setExcludedByPath(_ value: Bool, paths: [String]) async throws {
+    func setExcludedByPath(_ value: Bool, paths: [String]) throws {
         dump(paths, name: "paths")
         try TMUtilLauncher().setExcludedByPath(value, paths: paths)
         if UserDefaults.standard.bool(forKey: DefaultsKey.forceFakeFailureOnExcludeByPath) {
