eeab68c24a91693e652f22b82b7d1bcd9090b165
Grigory Entin <grigory.entin@gmail.com>
2022-02-08 17:21:20 +0100

    Cleaned up names in TMUtilHelperXPC.

--- Targets/TMBuddyShared/XPC/TMUtilHelperXPC.swift
+++ Targets/TMBuddyShared/XPC/TMUtilHelperXPC.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 @objc protocol TMUtilHelperXPC: CommonHelperXPC {
-    func setExcludedByPath(_ value: Bool, paths: [String], completion: @escaping (Error?) -> Void)
+    func setExcludedByPath(_ value: Bool, paths: [String], reply: @escaping (Error?) -> Void)
 }
