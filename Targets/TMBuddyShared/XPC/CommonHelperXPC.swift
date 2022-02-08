6c9a7b10c79899f2cbe0761196537e6699696d1e
Grigory Entin <grigory.entin@gmail.com>
2022-02-08 17:43:05 +0100

    Segregated XPC PostInstallController, adding ability to check whether the post install is needed.

--- Targets/TMBuddyShared/XPC/CommonHelperXPC.swift
+++ Targets/TMBuddyShared/XPC/CommonHelperXPC.swift
@@ -4,5 +4,6 @@ import Foundation
     func pingAsync(_: @escaping (Bool) -> Void)
     func versionAsync(_: @escaping (String) -> Void)
     func crashAsync(_: @escaping (Bool) -> Void)
-    func postInstall(sourceBundlePath: String, _: @escaping (Error?) -> Void)
+    func postInstallAsync(sourceBundlePath: String, reply: @escaping (Error?) -> Void)
+    func postInstallNeededAsync(sourceBundlePath: String, reply: @escaping (Error?, Bool) -> Void)
 }
