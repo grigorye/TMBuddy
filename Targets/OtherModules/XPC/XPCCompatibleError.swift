113c656b6c44f10d77dcb46fd556efaa0d9ee1b0
Grigory Entin <grigory.entin@gmail.com>
2022-02-09 06:23:40 +0100

    Added workaround for errors converted into NSErrors when being transported over XPC.

---
+++ Targets/OtherModules/XPC/XPCCompatibleError.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+//
+// A partial remedy for Swift.Error losing every Swift specific bit of information when transported over XPC.
+//
+protocol XPCCompatibleError: CustomNSError {}
+
+extension XPCCompatibleError where Self: LocalizedError {
+    
+    var errorUserInfo: [String : Any] {
+        [String : Any]() ≈ {
+            if let value = errorDescription {
+                $0[NSLocalizedDescriptionKey] = value
+            }
+            if let value = failureReason {
+                $0[NSLocalizedFailureReasonErrorKey] = value
+            }
+            if let value = recoverySuggestion {
+                $0[NSLocalizedRecoverySuggestionErrorKey] = value
+            }
+        }
+    }
+}
+
+extension XPCCompatibleError {
+    
+    var errorUserInfo: [String : Any] {
+        [NSLocalizedDescriptionKey: "\(self)"]
+    }
+}
