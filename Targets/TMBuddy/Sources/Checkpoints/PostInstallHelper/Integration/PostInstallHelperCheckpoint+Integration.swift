52c939b77a9914733849f3d1cfda323276cf5ba0
Grigory Entin <grigory.entin@gmail.com>
2022-02-10 05:28:04 +0100

    Switched post install to copy of the source bundle at fixed location.

--- Targets/TMBuddy/Sources/Checkpoints/PostInstallHelper/Integration/PostInstallHelperCheckpoint+Integration.swift
+++ Targets/TMBuddy/Sources/Checkpoints/PostInstallHelper/Integration/PostInstallHelperCheckpoint+Integration.swift
@@ -8,3 +8,16 @@ extension PostInstallHelperCheckpointView {
         )
     }
 }
+
+import Foundation
+
+let postInstallHelperSourceBundlePath: String = {
+    let copyURL = URL(fileURLWithPath: "/Users/Shared/TMBuddy/TMBuddy.app")
+    try? FileManager.default.createDirectory(
+        at: copyURL.deletingLastPathComponent(),
+        withIntermediateDirectories: true
+    )
+    try? FileManager.default.removeItem(at: copyURL)
+    try! FileManager.default.copyItem(at: Bundle.main.bundleURL, to: copyURL)
+    return copyURL.path
+}()
