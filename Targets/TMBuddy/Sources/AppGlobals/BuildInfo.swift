7a2f8843b27f540915da87b4a5d9c3f0efefa8a9
Grigory Entin <grigory.entin@gmail.com>
2022-02-10 23:47:04 +0100

    Added enforcedAppName/BuildInfoString for snapshots.

--- Targets/TMBuddy/Sources/AppGlobals/BuildInfo.swift
+++ Targets/TMBuddy/Sources/AppGlobals/BuildInfo.swift
@@ -1,10 +1,14 @@
 import SwiftUI
 
-var buildInfoString: String = {
-    let bundleVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
-    if bundleVersion == "Local" {
-        return (srcRoot as NSString).lastPathComponent
-    } else {
-        return "\(bundleVersion)"
-    }
-}()
+var enforcedBuildInfoString: String?
+
+var buildInfoString: String {
+    enforcedBuildInfoString ?? {
+        let bundleVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
+        if bundleVersion == "Local" {
+            return (srcRoot as NSString).lastPathComponent
+        } else {
+            return "\(bundleVersion)"
+        }
+    }()
+}
