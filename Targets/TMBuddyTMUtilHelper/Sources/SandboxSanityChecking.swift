39c70d927304ef29f668c29be221b384470ccab0
Grigory Entin <grigory.entin@gmail.com>
2022-02-10 04:56:24 +0100

    Tweaked order of checks in helper's sandbox sanity.

--- Targets/TMBuddyTMUtilHelper/Sources/SandboxSanityChecking.swift
+++ Targets/TMBuddyTMUtilHelper/Sources/SandboxSanityChecking.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 func checkSandboxSanity(writablePath: String, readOnlyPath: String) throws {
-    guard fileManager.isWritableFile(atPath: writablePath) == true else {
-        throw SandboxSanityCheckingError.fileIsNotWriteable(atPath: writablePath)
-    }
     guard fileManager.isWritableFile(atPath: readOnlyPath) == false else {
         throw SandboxSanityCheckingError.fileIsNotReadOnly(atPath: readOnlyPath)
+    }
+    guard fileManager.isWritableFile(atPath: writablePath) == true else {
+        throw SandboxSanityCheckingError.fileIsNotWriteable(atPath: writablePath)
     }
 }
 
