a855adfee6ce988a3704cfa639df15d35a4ec733
Grigory Entin <grigory.entin@gmail.com>
2022-02-12 21:29:09 +0100

    Added overriding isAppDistributedViaAppStore via enforcedIsAppDistributedViaAppStore.

--- Targets/OtherModules/Bundle/AppStoreDistribution.swift
+++ Targets/OtherModules/Bundle/AppStoreDistribution.swift
@@ -1,13 +1,17 @@
 import Foundation
 
-let isAppDistributedViaAppStore: Bool = {
-    guard let appStoreReceiptURL = Bundle.main.appStoreReceiptURL else {
-        return false
-    }
-    do {
-        return try appStoreReceiptURL.checkResourceIsReachable()
-    } catch {
-        dump(error, name: "expectedError")
-        return false
-    }
-}()
+var enforcedIsAppDistributedViaAppStore: Bool?
+
+var isAppDistributedViaAppStore: Bool {
+    enforcedIsAppDistributedViaAppStore ?? {
+        guard let appStoreReceiptURL = Bundle.main.appStoreReceiptURL else {
+            return false
+        }
+        do {
+            return try appStoreReceiptURL.checkResourceIsReachable()
+        } catch {
+            dump(error, name: "expectedError")
+            return false
+        }
+    }()
+}
