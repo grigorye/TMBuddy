db2d5b12be19b11c1284ad8b0863759530096584
Grigory Entin <grigory.entin@gmail.com>
2022-02-14 18:23:55 +0100

    Dropped MDItemBackupController in favor of ExtendedAttributesBackupController.

--- Targets/TMBuddyFinder/Sources/Helpers/DirectLookup/MetadataReader.swift
+++ Targets/TMBuddyFinder/Sources/Helpers/DirectLookup/MetadataReader.swift
@@ -4,11 +4,8 @@ protocol MetadataReader {
     func excludedBasedOnMetadata(_ url: URL) -> Bool
 }
 
-extension MDItemBackupController: MetadataReader {}
 extension ExtendedAttributesBackupController: MetadataReader {}
 
 var metadataReader: MetadataReader {
-    UserDefaults.standard.bool(forKey: DefaultsKey.forceMDItemMetadataReader)
-    ? MDItemBackupController()
-    : ExtendedAttributesBackupController()
+    ExtendedAttributesBackupController()
 }
