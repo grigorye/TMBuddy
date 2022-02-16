cdfaed297dc495adcc746e16df0efed7a3f0e3b9
Grigory Entin <grigory.entin@gmail.com>
2022-02-16 18:28:41 +0100

    Switched testing sticky exclusions to the generated test files.

--- Targets/TMBuddyTests/DirectLookupBasedStatusProviderTests.swift
+++ Targets/TMBuddyTests/DirectLookupBasedStatusProviderTests.swift
@@ -4,13 +4,34 @@ class DirectLookupBasedStatusProviderTests: XCTestCase {
     
     var provider: DirectLookupBasedStatusProvider { .init() }
     
+    let metadataWriter: MetadataWriter = ExtendedAttributesBackupController()
+    
+    var testDirectory: URL!
+    var stickyExcludedDirectory: URL!
+    var stickyExcludedDirectoryChild: URL!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        let fileManager = FileManager.default
+        testDirectory = fileManager.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        stickyExcludedDirectory = testDirectory.appendingPathComponent("StickyExcluded")
+        stickyExcludedDirectoryChild = stickyExcludedDirectory.appendingPathComponent("Child")
+        try fileManager.createDirectory(at: stickyExcludedDirectoryChild, withIntermediateDirectories: true, attributes: nil)
+        addTeardownBlock { [testDirectory = testDirectory!] in
+            try! fileManager.removeItem(at: testDirectory)
+        }
+        
+        try metadataWriter.setExcluded(true, urls: [stickyExcludedDirectory])
+    }
+    
     func testExcludedBasedOnMetadata() async throws {
-        let status = try provider.statusForItem(URL(fileURLWithPath: NSHomeDirectory() + "/TMBuddyTest/stickyExcluded"))
+        let status = try provider.statusForItem(stickyExcludedDirectory)
         XCTAssertEqual(status, .stickyExcluded)
     }
     
     func testExcludedBasedOnParentMetadata() async throws {
-        let status = try provider.statusForItem(URL(fileURLWithPath: NSHomeDirectory() + "/TMBuddyTest/stickyExcluded/parentExcluded"))
+        let status = try provider.statusForItem(stickyExcludedDirectoryChild)
         XCTAssertEqual(status, .parentExcluded)
     }
     
