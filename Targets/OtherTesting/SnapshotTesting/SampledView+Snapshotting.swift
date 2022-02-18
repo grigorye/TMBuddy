401a4f512459f7da478ed982b2b144c769b3122d
Grigory Entin <grigory.entin@gmail.com>
2022-02-18 04:25:38 +0100

    Added support for fixed-width snapshots.

--- Targets/OtherTesting/SnapshotTesting/SampledView+Snapshotting.swift
+++ Targets/OtherTesting/SnapshotTesting/SampledView+Snapshotting.swift
@@ -1,17 +1,29 @@
+import SwiftUI
 import SnapshotTesting
 
 extension SampledView {
     
     static func snapshotSamples(
+        frameWidth: CGFloat? = nil,
         record recording: Bool,
         file: StaticString = #file,
         testName: String = #function,
         line: UInt = #line
     ) {
         for sample in State.Sample.allCases {
+            guard #available(macOS 12.0, *) else {
+                fatalError()
+            }
+            
             let view = Self.new()
                 .environment(Self.sampleStateEnvironmentValuesKeyPath, sample.state)
-                .border(.red)
+                .overlay {
+                    Rectangle()
+                        .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [2]))
+                }
+                .frame(width: frameWidth, alignment: .leading)
+                .padding()
+                .border(.blue)
 
             assertSnapshot(
                 matching: view,
