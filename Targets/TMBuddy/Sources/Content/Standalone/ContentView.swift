92df8b638a08a39f086f0a01fe7a2488f5f3ff73
Grigory Entin <grigory.entin@gmail.com>
2022-02-20 22:33:31 +0100

    Fixed missing padding around tab view.

--- Targets/TMBuddy/Sources/Content/Standalone/ContentView.swift
+++ Targets/TMBuddy/Sources/Content/Standalone/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                         Text("Legend")
                     }
             }
+            .padding()
         } else {
             VStack(alignment: .leading) {
                 Text("Setup")
