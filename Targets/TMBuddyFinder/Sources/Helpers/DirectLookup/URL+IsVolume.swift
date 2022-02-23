34b7e3c3face437a77213139fe1914be4ca33599
Grigory Entin <grigory.entin@gmail.com>
2022-02-23 02:10:38 +0100

    Added support for explicit querying volume exclusion status.

--- Targets/TMBuddyFinder/Sources/Helpers/DirectLookup/URL+IsVolume.swift
+++ Targets/TMBuddyFinder/Sources/Helpers/DirectLookup/URL+IsVolume.swift
@@ -1,7 +1,21 @@
 import Foundation
 
 extension URL {
-    func isVolume() throws -> Bool {
-        return try resourceValues(forKeys: [.isVolumeKey]).isVolume == true
+    func isVolume() -> Bool {
+        do {
+            return try resourceValues(forKeys: [.isVolumeKey]).isVolume == true
+        } catch {
+            dump((error, path: self.path), name: "isVolumeResourceValueFailed")
+            return false
+        }
+    }
+    
+    func volumeUUID() -> String? {
+        do {
+            return try resourceValues(forKeys: [.volumeUUIDStringKey]).volumeUUIDString
+        } catch {
+            dump((error, path: self.path), name: "volumeUUIDStringResourceValueFailed")
+            return nil
+        }
     }
 }
