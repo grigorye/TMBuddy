5f0c4743c2f7e7314413d376f1504ddc9ce08be7
Grigory Entin <grigory.entin@gmail.com>
2022-03-01 03:14:54 +0100

    Switched to legend with badged icons.

--- Targets/TMBuddy/Sources/Legend/LegendView.swift
+++ Targets/TMBuddy/Sources/Legend/LegendView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import AppKit
 
 struct LegendView: View {
     
@@ -6,21 +7,71 @@ struct LegendView: View {
         let cases = TMStatus.allCases.filter { [.included, .unknown].contains($0) == false }
         VStack(alignment: .leading) {
             ForEach(cases, id: \.self) { status in
+                let sampleImage = sampleImageForStatus(status)
+                let badge = Image(nsImage: imageForStatus(status)!)
+                let label = localizedStatus(status)
+
                 HStack {
-                    if let image = imageForStatus(status) {
-                        Image(nsImage: image)
-                    } else {
-                        let placeholderImage = imageForStatus(.stickyExcluded)!
-                        Image(nsImage: placeholderImage)
-                            .hidden()
-                    }
-                    let label = localizedStatus(status)
+                    ImageWithBadge(image: sampleImage, badge: badge, scale: 0.4)
                     Text(label)
                 }
             }
         }
         .padding()
         .fixedSize()
+    }
+}
+
+func sampleImageForStatus(_ status: TMStatus) -> Image {
+    let sampleIcon: NSImage = {
+        switch status {
+        case .unsupportedVolume:
+            return volumeIcon
+        case .excludedVolume:
+            return volumeIcon
+        case .included:
+            return folderIcon
+        case .parentExcluded:
+            return folderIcon
+        case .stickyExcluded:
+            return folderIcon
+        case .pathExcluded:
+            return folderIcon
+        case .unknown:
+            return folderIcon
+        }
+    }()
+
+    let sampleNSImage = sampleIcon ≈ {
+        $0.size = .init(width: 128, height: 128)
+    }
+    let sampleImage = Image(nsImage: sampleNSImage)
+    return sampleImage
+}
+
+var folderIcon: NSImage {
+    NSWorkspace.shared.icon(forFile: FileManager.default.temporaryDirectory.path)
+}
+
+var volumeIcon: NSImage {
+    NSWorkspace.shared.icon(forFile: "/")
+}
+
+struct ImageWithBadge: View {
+    
+    let image: Image
+    let badge: Image
+    var scale: CGFloat = 1
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            image
+                .resizable()
+                .frame(width: 128 * scale, height: 128 * scale)
+            badge
+                .resizable()
+                .frame(width: 40 * scale, height: 40 * scale)
+        }
     }
 }
 
