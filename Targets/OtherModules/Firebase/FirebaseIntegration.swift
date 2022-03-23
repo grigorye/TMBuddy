6583c3e4e94a08b16aee2051202454d0344cd6fd
Grigory Entin <grigory.entin@gmail.com>
2022-03-23 20:52:44 +0100

    Integrated FirebaseAnalyticsEnabler.

--- Targets/OtherModules/Firebase/FirebaseIntegration.swift
+++ Targets/OtherModules/Firebase/FirebaseIntegration.swift
@@ -4,8 +4,10 @@ import Firebase
 
 func activateFirebase() {
     _ = configureFirebaseAppOnce
+    _ = firebaseAnalyticsEnabler
 }
 
 let configureFirebaseAppOnce: Void = FirebaseApp.configure()
+let firebaseAnalyticsEnabler = FirebaseAnalyticsEnabler()
 
 #endif
