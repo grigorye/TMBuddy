35d3693efbf7c198338ad921a5550eba6ded7f38
Grigory Entin <grigory.entin@gmail.com>
2022-04-03 02:04:54 +0200

    Enforced custom logging for GoogleUtilities.

--- Targets/OtherModules/Firebase/FirebaseIntegration.swift
+++ Targets/OtherModules/Firebase/FirebaseIntegration.swift
@@ -1,13 +1,22 @@
 #if canImport(Firebase)
 
 import Firebase
+import GoogleUtilities_Logger
 
 func activateFirebase() {
     _ = configureFirebaseAppOnce
     _ = firebaseAnalyticsEnabler
 }
 
-let configureFirebaseAppOnce: Void = FirebaseApp.configure()
+let customGULLogImp: GULLogImp = { (level: GoogleLoggerLevel, version: String, service: String, messageCode: String, message: String) in
+    dump((service: service, message: message), name: "GULLog")
+}
+
+let configureFirebaseAppOnce: Void = {
+    GULLoggerConfig.logImp = customGULLogImp
+    FirebaseApp.configure()
+}()
+
 let firebaseAnalyticsEnabler = FirebaseAnalyticsEnabler()
 
 #endif
