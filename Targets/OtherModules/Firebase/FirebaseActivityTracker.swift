1bc8a7bb18cacc10e9d586690c92bc9c4c437328
Grigory Entin <grigory.entin@gmail.com>
2022-04-04 07:08:46 +0200

    Added a bit of tracing for FirebaseTrackedActivity.

--- Targets/OtherModules/Firebase/FirebaseActivityTracker.swift
+++ Targets/OtherModules/Firebase/FirebaseActivityTracker.swift
@@ -9,24 +9,42 @@ struct FirebaseActivityTracker: ActivityTracker {
     }
     
     func beginActivity<T>(_ value: T, name: String, sourceInfo: SourceInfo) -> TrackedActivity {
+        dump(name, name: "")
         guard let trace = Performance.startTrace(name: name) else {
             assertionFailure()
-            return DummyTrackedActivity()
+            return DummyTrackedActivity(name: name)
         }
-        return FirebaseTrackedActivity(trace: trace)
+        return FirebaseTrackedActivity(trace: trace, name: name)
     }
 }
 
 struct FirebaseTrackedActivity: TrackedActivity {
     let trace: Trace
+    let name: String
+    
+    init(trace: Trace, name: String) {
+        self.trace = trace
+        self.name = name
+        
+        dump(name, name: "")
+    }
     
     func endActivity<T>(_ value: T, sourceInfo: (SourceInfo)) {
+        dump(name, name: "")
         trace.stop()
     }
 }
 
 struct DummyTrackedActivity: TrackedActivity {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+        
+        dump(name, name: "")
+    }
     func endActivity<T>(_ value: T, sourceInfo: (SourceInfo)) {
+        dump(name, name: "")
     }
 }
 
