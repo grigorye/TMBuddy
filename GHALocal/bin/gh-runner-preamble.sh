09233a6ede5491ba77482e70cbf77943853a201d
Grigory Entin <grigory.entin@gmail.com>
2022-06-02 02:00:12 +0200

    Added support for GHA_LOCAL_PREAMBLE.

--- GHALocal/bin/gh-runner-preamble.sh
+++ GHALocal/bin/gh-runner-preamble.sh
@@ -3,3 +3,5 @@
 if [ -n "${KEYCHAIN_PATH:-}" ] && [ -f "$KEYCHAIN_PATH" ]; then
     security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
 fi
+
+[ -z "${GHA_LOCAL_PREAMBLE:-}" ] || "$GHA_LOCAL_PREAMBLE"
