4195b1c12bc2806501ee9e5f11068c95911350ca
Grigory Entin <grigory.entin@gmail.com>
2023-03-05 05:24:38 +0100

    Updated for PrivilegedHelperManager.

--- Targets/TMBuddy/Sources/Checkpoints/SMJobBless/Integration/SMJobBlessCheckpoint+ActionHandler.swift
+++ Targets/TMBuddy/Sources/Checkpoints/SMJobBless/Integration/SMJobBlessCheckpoint+ActionHandler.swift
@@ -28,10 +28,12 @@ final class SMJobBlessCheckpointActionHandler: SMJobBlessCheckpointActions, Trac
         let message = String(localized: "\(appName) uses the helper tool for changing Time Machine exclusion paths on behalf of the administrator.")
         let icon = Bundle.main.url(forResource: "bless", withExtension: "png")
         do {
-            try LaunchdManager.authorizeAndBless(message: message, icon: icon)
+            try privilegedHelperManager.authorizeAndBless(message: message, icon: icon)
         } catch {
             dump(error, name: "blessFailed")
             NSApplication.shared.presentError(error)
         }
     }
+    
+    let privilegedHelperManager = PrivilegedHelperManager.shared
 }
