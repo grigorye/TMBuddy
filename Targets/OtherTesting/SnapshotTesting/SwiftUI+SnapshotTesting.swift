052ed67d2f7f50f9e9c11153970c043865b6bcd2
Grigory Entin <grigory.entin@gmail.com>
2023-04-29 18:38:16 +0200

    Made Swift UI views snapshotted inside NSWindow.

--- Targets/OtherTesting/SnapshotTesting/SwiftUI+SnapshotTesting.swift
+++ Targets/OtherTesting/SnapshotTesting/SwiftUI+SnapshotTesting.swift
@@ -13,9 +13,20 @@ func assertSnapshot<View: SwiftUI.View>(
     let hostingView = NSHostingView(rootView: view)
     let fittingSize = hostingView.fittingSize
     hostingView.bounds.size = fittingSize
-    assertSnapshot(
-        matching: hostingView,
-        as: .image(size: fittingSize),
+    let window = NSWindow(
+        contentRect: .init(origin: .zero, size: fittingSize),
+        styleMask: .borderless,
+        backing: .buffered,
+        defer: false,
+        screen: .main
+    ) … {
+        $0.backgroundColor = .clear
+        $0.isReleasedWhenClosed = false
+    }
+    window.contentView = hostingView
+    try assertSnapshot(
+        matching: NSWindow.snapshot(windowNumbers: [window.windowNumber], imageOptions: [.bestResolution]),
+        as: .image(scaleFactor: window.backingScaleFactor),
         named: name,
         record: recording,
         file: file,
