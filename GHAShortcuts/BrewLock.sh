416fec36c0ad28604de713262e879af010f3388c
Grigory Entin <grigory.entin@gmail.com>
2023-06-03 03:43:40 +0200

    Added workaround for using brew with non-admin user.

--- GHAShortcuts/BrewLock.sh
+++ GHAShortcuts/BrewLock.sh
@@ -6,3 +6,17 @@ while ! shlock -f "$lckfile" -p $$; do
     sleep 1
 done
 trap 'rm -f "$lckfile"' EXIT
+
+brew=$(command -v brew)
+
+if [ "$brew" == "/opt/homebrew/bin/brew" ]; then
+    # Workaround user not being the owner of Homebrew (particularly, being an extra user on orchard-controlled VM).
+    id=$(id -u)
+    brew_owner_id=$(stat -f '%u' "$brew")
+
+    if [ "$id" != "$brew_owner_id" ]; then
+        brew() {
+            sudo -u admin command brew "$@"
+        }
+    fi
+fi
