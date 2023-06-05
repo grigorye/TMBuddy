f2474a29dfe4b1c540bb6db5f62ba1021540786b
Grigory Entin <grigory.entin@gmail.com>
2023-06-05 03:28:41 +0200

    Employed GE_SNAPSHOT_TESTING for snapshot dependencies.

--- Targets/TMBuddy/Sources/AppGlobals/DisplayNames.swift
+++ Targets/TMBuddy/Sources/AppGlobals/DisplayNames.swift
@@ -5,19 +5,25 @@ private let fileManager = FileManager.default
 @MainActor var enforcedAppName: String? { defaultEnforcedAppName }
 
 @MainActor var appName: String {
-    enforcedAppName ?? {
-        fileManager.displayName(atPath: Bundle.main.bundlePath)
-    }()
+#if !GE_SNAPSHOT_TESTING
+    fileManager.displayName(atPath: Bundle.main.bundlePath)
+#else
+    "TMBuddy"
+#endif
 }
 
 let finderName = fileManager.displayName(atPath: "/System/Library/CoreServices/Finder.app")
 
 @MainActor var plugInName: String {
+#if !GE_SNAPSHOT_TESTING
     guard let plugInURL = plugInURL else {
         dump((), name: "unknownPlugInURL")
         return plugInFallbackName
     }
     return fileManager.displayName(atPath: plugInURL.path)
+#else
+    return "\(appName) Finder Extension"
+#endif
 }
 
 @MainActor private var plugInFallbackName: String {
