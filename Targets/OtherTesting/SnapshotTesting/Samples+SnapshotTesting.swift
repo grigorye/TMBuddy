59727c022743c2c37803a1d71a73cae8d996f873
Grigory Entin <grigory.entin@gmail.com>
2023-06-18 21:42:33 +0200

    Extracted Paths+SnapshotTesting.swift.

--- Targets/OtherTesting/SnapshotTesting/Samples+SnapshotTesting.swift
+++ Targets/OtherTesting/SnapshotTesting/Samples+SnapshotTesting.swift
@@ -93,7 +93,6 @@ extension Snapshotting {
     }
 }
 
-
 func suffixForScaleFactor(_ scaleFactor: CGFloat) -> String? {
     guard scaleFactor != 1.0 else {
         return nil
@@ -104,19 +103,4 @@ func suffixForScaleFactor(_ scaleFactor: CGFloat) -> String? {
     default:
         return "@\(scaleFactor)x"
     }
-}
-
-func snapshotDirectory(forFile file: String) -> String {
-    let fileUrl = URL(fileURLWithPath: file, isDirectory: false)
-    let snapshotDirectoryUrl = fileUrl
-        .deletingPathExtension()
-        .appendingPathComponent(environmentSpecificRelativeSnapshotsPath())
-    return snapshotDirectoryUrl.path
-}
-
-func environmentSpecificRelativeSnapshotsPath() -> String {
-    let osVersion = ProcessInfo.processInfo.operatingSystemVersion
-    return [
-        "macOS-\(osVersion.majorVersion).\(osVersion.minorVersion)"
-    ].joined(separator: "/")
 }
