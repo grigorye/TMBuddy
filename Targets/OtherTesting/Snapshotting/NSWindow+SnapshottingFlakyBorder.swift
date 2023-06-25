eacc7a465f3421ff477c42bddd7b5da2841dc077
Grigory Entin <grigory.entin@gmail.com>
2023-06-25 02:21:43 +0200

    Fixed modification date not updated for UITestSnapshots.

--- Targets/OtherTesting/Snapshotting/NSWindow+SnapshottingFlakyBorder.swift
+++ Targets/OtherTesting/Snapshotting/NSWindow+SnapshottingFlakyBorder.swift
@@ -2,9 +2,15 @@ import AppKit
 
 @MainActor
 func snapshotFlakyBorderWindow(window: NSWindow, snapshotBaseURL: URL) {
-    // Snapshot shadowless, and *bail out* unless it changed.
+    let borderlessSnapshotURL = snapshotBaseURL.appendingPathExtension("borderless.png")
+    let shadowSnapshotURL = snapshotBaseURL.appendingPathExtension("png")
+    defer {
+        try? FileManager.default.setAttributes([.modificationDate: Date()], ofItemAtPath: borderlessSnapshotURL.path)
+        try? FileManager.default.setAttributes([.modificationDate: Date()], ofItemAtPath: shadowSnapshotURL.path)
+    }
+    // Snapshot shadowless, and *bail out* unless it's changed.
     do {
-        let snapshotURL = snapshotBaseURL.appendingPathExtension("borderless.png")
+        let snapshotURL = borderlessSnapshotURL
         let snapshot = try! window.snapshot(options: [.bestResolution, .boundsIgnoreFraming])
         let newBitmapRep = NSBitmapImageRep(data: snapshot.tiffRepresentation!)!.representation(using: .png, properties: [:])!
         if let oldBitmapRep = try? Data(contentsOf: snapshotURL) {
@@ -18,7 +24,7 @@ func snapshotFlakyBorderWindow(window: NSWindow, snapshotBaseURL: URL) {
     
     // Take another snapshot variant, now with shadow.
     do {
-        let snapshotURL = snapshotBaseURL.appendingPathExtension("png")
+        let snapshotURL = shadowSnapshotURL
         let snapshot = try! window.snapshot(options: [.bestResolution])
         let newBitmapRep = NSBitmapImageRep(data: snapshot.tiffRepresentation!)!.representation(using: .png, properties: [:])!
         try! newBitmapRep.write(to: snapshotURL)
